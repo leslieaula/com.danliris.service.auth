c7a2dae5493b4a3f37b088bf2c0279027113f063
jackyruslymoonlay <jacky.rusly@moonlay.com>
2018-01-26 15:30:20 +0700

    Remove Folder

--- Com.Danliris.Service.Auth.Lib/Interfaces/IMap.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Com.DanLiris.Service.Auth.Lib.Interfaces
-{
-    public interface IMap<TModel, TViewModel>
-    {
-        TViewModel MapToViewModel(TModel model);
-
-        TModel MapToModel(TViewModel viewModel);
-    }
-}
