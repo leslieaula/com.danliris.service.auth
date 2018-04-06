b9e193919d8c24822c5b292f9309db7ccb0ca29a
Leslie Aula <leslieaula@users.noreply.github.com>
2018-04-06 16:03:41 +0700

    Delete Dockerfile.test.build.bat

--- Dockerfile.test.build.bat
+++
@@ -1,5 +0,0 @@
-docker build -f Dockerfile.test.build -t com-danliris-service-auth-webapi:test-build .
-docker create --name com-danliris-service-auth-webapi-test-build com-danliris-service-auth-webapi:test-build
-mkdir bin
-docker cp com-danliris-service-auth-webapi-test-build:/out/. ./bin/publish
-docker build -f Dockerfile.test -t com-danliris-service-auth-webapi:test .
