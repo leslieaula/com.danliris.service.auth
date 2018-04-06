a9b053f3cb652ec6ae15e244df5f38fe0cf2e147
Leslie Aula <leslieaula@users.noreply.github.com>
2018-04-06 15:52:44 +0700

    Update Dockerfile.test.build.sh

--- Dockerfile.test.build.sh
+++ Dockerfile.test.build.sh
@@ -1,5 +1,5 @@
 docker build -f Dockerfile.test.build -t com-danliris-service-auth-webapi:test-build .
 docker create --name com-danliris-service-auth-webapi-test-build com-danliris-service-auth-webapi:test-build
-mkdir bin
+mkdir -p bin
 docker cp com-danliris-service-auth-webapi-test-build:/out/. ./bin/publish
 docker build -f Dockerfile.test -t com-danliris-service-auth-webapi:test .
